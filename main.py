6e3cb08c5a3eb6858a167d80eb11b28effc7be17
luanvfm <luanventurafm2@gmail.com>
2026-04-26 13:09:03 -0300

    feat chrome drivers

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.chromium.service import Service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
+driver = webdriver.chrome(service=Service(ChromeDriverManager.install()))
 service = Service(executable_path="")
 driver = webdriver.Chrome(service=service)
+
+webdriver.chromium
+
+# Entering the correct class page
+
+# Check if there is a new div with  
